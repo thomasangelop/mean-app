9dcaeb7ee6d1bb20305a96370ca26349f7255b3a
thomasangelop <me@thomasagnelo.codes>
2019-07-11 13:51:19 -0500

    adding validation for Post

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,5 +1,7 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 
+import {Post} from '../post.model';
+
 @Component ({
   selector: 'app-post-create',
   templateUrl: './post-create.component.html',
@@ -11,7 +13,7 @@ export class PostCreateComponent {
   @Output() postCreated = new EventEmitter();
 
   onAddPost() {
-    const post = {
+    const post: Post = {
       title: this.enteretedTitle,
       content: this.enteredContent,
     };
--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,12 +1,14 @@
 import {Component, Input } from '@angular/core';
 
+import { Post } from '../post.model';
+
 @Component ({
   selector: 'app-post-list',
   templateUrl: './post-list.component.html',
   styleUrls: ['./post-list.component.css'],
 })
 export class PostListComponent {
-  @Input() posts = [];
+  @Input() posts: Post[] = [];
   // posts = [
   //   {tite: 'Fist Post', content: 'This is the first post content'},
   //   {tite: 'Second Post', content: 'This is the first post content'},
