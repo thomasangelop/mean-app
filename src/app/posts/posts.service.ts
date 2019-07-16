fcc583d278d9248d39f2dbf57636eb27e355e246
thomasangelop <me@thomasagnelo.codes>
2019-07-16 16:30:57 -0500

    delete is working on refresh

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -46,4 +46,14 @@ export class PostsService {
         this.postsUpdated.next([...this.posts]);
       });
   }
+
+  deletePost(postId: string) {
+    this.http.delete('http://localhost:3000/api/posts/' + postId)
+      .subscribe(() => {
+        console.log('Post Deleted!');
+        const updatedPosts = this.posts.filter(post => post.id !== postId);
+        this.posts = updatedPosts;
+        this.postsUpdated.next([...this.posts]);
+      });
+  }
 }
