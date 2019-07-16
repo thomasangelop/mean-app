da56504f10469659e1aed75df27780f0b41b2cb6
thomasangelop <me@thomasagnelo.codes>
2019-07-16 12:47:59 -0500

    server.js setup with express

--- backend/app.js
+++ backend/app.js
@@ -2,3 +2,13 @@ const express = require('express');
 
 const app = express();
 
+app.use((req, res, next) => {
+  console.log('first middleware');
+  next(); //call next if not sending a response
+});
+
+app.use((req, res, next) => {
+  res.send('hello from express!');
+});
+
+module.exports = app;
