10b4b6f5fa3394106c97728697b782776dacb402
thomasangelop <me@thomasagnelo.codes>
2019-07-16 13:19:40 -0500

    frontend connected to backend successfully. woot

--- backend/app.js
+++ backend/app.js
@@ -2,13 +2,35 @@ const express = require('express');
 
 const app = express();
 
+//since local host and app are being serverd on different ports
 app.use((req, res, next) => {
-  console.log('first middleware');
-  next(); //call next if not sending a response
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Header',
+    'Origin, X-Requested-With, Content-Type, Accept'
+    );
+  res.setHeader('Access-Control-Allow-Methods',
+    'GET, POST, PATCH, DELETE, OPTIONS');
+  next();
 });
 
-app.use((req, res, next) => {
-  res.send('hello from express!');
+app.use('/api/posts', (req, res, next) => {
+  const posts = [
+    {
+      id: '1',
+      title: 'Yo',
+      content: 'yeah!'
+    },
+    {
+      id: '2',
+      title: 'Yos',
+      content: 'yeahs!'
+    },
+
+  ];
+  res.status(200).json({
+    message: 'Posts fetched successfully!',
+    posts: posts
+  });
 });
 
 module.exports = app;
--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 import { Subject }from 'rxjs';
 
 import { Post } from './post.model';
@@ -9,9 +10,14 @@ export class PostsService {
   private posts: Post[] = [];
   private postsUpdated = new Subject<Post[]>();
 
+  constructor(private http: HttpClient) {}
+
   getPosts() {
-    // create a new array with the old objects
-    return [...this.posts];
+    this.http.get<{message: string, posts: Post[]}>('http://localhost:3000/api/posts')
+      .subscribe((postData) => {
+        this.posts = postData.posts;
+        this.postsUpdated.next([...this.posts]);
+      });
   }
 
   getPostUpdateListener() {
@@ -19,7 +25,7 @@ export class PostsService {
   }
 
   addPost(title: string, content: string) {
-    const post: Post = {title: title, content: content};
+    const post: Post = { id: null, title: title, content: content };
     this.posts.push(post);
     this.postsUpdated.next([...this.posts]);
   }
